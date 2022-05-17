f202f8100ddfd271f37747795346a2575e095348
rtpascual <roshanepascual@gmail.com>
2022-05-16 19:09:56 -0700

    Added reducers to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import dividendReducer from "../features/dividends/dividendSlice";
 
 export const store = configureStore({
   reducer: {
