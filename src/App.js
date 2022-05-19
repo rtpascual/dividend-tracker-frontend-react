d55c2e80b1ea223a6a1f0cdf3dc8f437695c8514
rtpascual <roshanepascual@gmail.com>
2022-05-18 22:02:56 -0700

    clean up files and add CSS variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
-import "./App.css";
 
 export default function App() {
   return (
